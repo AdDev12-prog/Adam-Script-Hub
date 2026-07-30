aa76a15558e89dde246ab1af5cad42e2430e548b
AdDev12-prog <domikhorshid984@gmail.com>
2026-07-30 19:43:37 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -28,15 +28,18 @@ const scripts = [
     // ==========================================
 
     {
-        game: "🚔 Jailbreak",
+        game: "🚔 Jailbreak (UNIVERSAL FARM)",
 
-        description: "Auto Farm, Auto Rob.",
+        description: "Auto Farm, Auto Rob, Auto Arrest",
 
         updated: "29 Jul 2026",
 
         badge: "UPDATED",
 
-        script: `loadstring(game:HttpGet("YOUR_LINK"))()`
+        script: `--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+loadstring(game:HttpGet('https://raw.githubusercontent.com/BlitzIsKing/UniversalFarm/main/Loader/Regular'))()`
     },
 
 
@@ -46,15 +49,15 @@ const scripts = [
     // ==========================================
 
     {
-        game: "🌱 Grow a Garden",
+        game: "🌱 Grow a Garden 2",
 
-        description: "Auto Farm and Auto Collect.",
+        description: "Auto Farm and Auto Collect, Auto Harvest",
 
         updated: "28 Jul 2026",
 
         badge: "HOT",
 
-        script: `loadstring(game:HttpGet("YOUR_LINK"))()`
+        script: `loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/97c3f6db55a2cf72141537a85458e5a7.lua"))()`
     },
 
 
@@ -64,7 +67,7 @@ const scripts = [
     // ==========================================
 
     {
-    game: "",
+    game: "Grow A Garden",
 
     badge: "NEW",
 
@@ -73,6 +76,6 @@ const scripts = [
     description:
         "Write anything here.",
 
-    script: `-- Paste your Lua script here`
+    script: `loadstring(game:HttpGet("https://raw.githubusercontent.com/Skibidiking123/Fisch1/refs/heads/main/FischMain"))()`
     },
 ];
